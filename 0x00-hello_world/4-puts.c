011f9c226f0cbd581cac35349c2fb754aca64ea2
AbdalrhmanShazally <ashazally@hotmail.com>
2023-08-10 00:04:07 -0700

    fix traling white space

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /*main method begin*/
-int main(void) 
+int main(void)
 {
-	/* using Puts to print the recommended output  */
-	puts("\"Programming is like building a multilingual puzzle");
-	/* Program Ends with Zero as requested */
-	return (0);
+/* using Puts to print the recommended output  */
+puts("\"Programming is like building a multilingual puzzle");
+/* Program Ends with Zero as requested */
+return (0);
 }
