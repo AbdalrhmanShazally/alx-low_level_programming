8e86fc1e94d42e9d353a5ef4fa814f8549089960
AbdalrhmanShazally <ashazally@hotmail.com>
2023-08-10 00:17:26 -0700

    add 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *  Description: 'Print exactly needed output for C programing tutorial ALX'
+ *  Return: Always 0 (Success)
+**/
+int main(void)
+{
+/* using printf to print the recommended output  */
+printf("with proper grammar, but the outcome is a piece of art,\n");
+/* Program Ends with Zero as requested */
+return (0);
+}
