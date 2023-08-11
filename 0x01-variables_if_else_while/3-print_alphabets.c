4da6f797603f0a3a9f2ca07495dc27cd215858b5
AbdalrhmanShazally <ashazally@hotmail.com>
2023-08-11 16:50:16 -0700

    fix Betty Error

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,25 +4,25 @@
 /* more headers goes there */
 /**
  *  * main - Entry point
-*  Description:Print alphabet 
+*  Description:Print alphabet
 *	In Lower/Upper Case and followed by new line
  *  Return: Always 0 (Success)
 **/
 int main(void)
 {
 	char lowercase = 'a';
-    char uppercase = 'A';
+	char uppercase = 'A';
 
     while (lowercase <= 'z')
 {
-        putchar(lowercase);
-        lowercase++;
+    putchar(lowercase);
+    lowercase++;
 }
 
     while (uppercase <= 'Z')
 {
-        putchar(uppercase);
-        uppercase++;
+    putchar(uppercase);
+    uppercase++;
 }
     putchar('\n');
 	return (0);
