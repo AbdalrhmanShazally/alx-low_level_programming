78383800e0d51dbc1e72d6a13291ffe049bb61a7
AbdalrhmanShazally <ashazally@hotmail.com>
2023-08-11 16:56:56 -0700

    add file 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+ *  * main - Entry point
+*  Description:Print alphabet
+*	In Lower/Upper Case and followed by new line
+ *  Return: Always 0 (Success)
+**/
+int main(void)
+{
+	char lowercase = 'a';
+
+	while (lowercase <= 'z')
+{
+	putchar(lowercase);
+	lowercase++;
+}
+	putchar('\n');
+	return (0);
+}
