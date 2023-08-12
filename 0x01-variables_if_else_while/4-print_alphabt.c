b7e20189a8acadee086c5f910a8f2f042e85d19e
AbdalrhmanShazally <ashazally@hotmail.com>
2023-08-11 17:07:13 -0700

    fix file 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,9 +12,9 @@ int main(void)
 {
 	char lowercase = 'a';
 
-	while (lowercase <= 'z')
+	while(lowercase <= 'z')
 {
-    if (lowercase != 'e' && lowercase != 'q')
+	if (lowercase != 'e' && lowercase != 'q')
 {
 	putchar(lowercase);
 }
