2c76ac740b128cfec7f41997870bdc7c56a5f5c2
AbdalrhmanShazally <ashazally@hotmail.com>
2023-08-11 17:21:36 -0700

    add file 5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+ *  * main - Entry point
+*	Description:prints all single digit
+*	numbers of base 10 starting from 0
+*	Return: Always 0 (Success)
+**/
+int main(void)
+{
+	int digit = 0;
+
+	while (digit < 10)
+{
+	printf("%d",digit);
+	digit++;
+}
+	putchar('\n');
+	return (0);
+}
