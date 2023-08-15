53d8e0414198661f1aab7da6a3ddd38e32068dc4
AbdalrhmanShazally <ashazally@hotmail.com>
2023-08-15 15:40:55 -0700

    add file 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *	_abs - Entry Point
+ *  Description: _abs fucntion return number absolute
+ *      @n: is the number that will be absolute
+ *      Return: absolute number
+ *      by a.shazally.
+**/
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
