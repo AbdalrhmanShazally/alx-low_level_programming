ccee16b1c14e38a0d8cd2c74c4c033e796952992
AbdalrhmanShazally <ashazally@hotmail.com>
2023-08-14 21:36:39 -0700

    add file 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *  Description: 0. _putchar
+ *  Return: Always 0 (Success)
+**/
+int main(void)
+{
+/* using printf to print the recommended output  */
+printf("_putchar\n");
+/* Program Ends with Zero as requested */
+return (0);
+}
