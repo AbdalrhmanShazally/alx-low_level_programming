e1283155957a2ab6c4107eb83637412fa9a47cf0
AbdalrhmanShazally <ashazally@hotmail.com>
2023-08-15 16:58:11 -0700

    add file 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *      add - Entry Point
+ *  Description: add fucntion add 2 integers and return result.
+ *	@a : first integers.
+ *	@b : secoend Integer.
+ * 	Return : result of adding 2 integers.
+ *      by a.shazally.
+**/
+int add(int a, int b)
+{
+int result;
+
+result = a + b;
+return (result);
+}
