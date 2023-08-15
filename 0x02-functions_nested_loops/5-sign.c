30ac679e1e25cf0924643a5e320954e9fa654289
AbdalrhmanShazally <ashazally@hotmail.com>
2023-08-15 15:45:04 -0700

    fix 5 sign.c betty white space

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
  *	print_sign - Entry Point
  *  Description: print_sign fucntion checks for Number Sign
  *      @n: is the number that will be checked
- *      Return: 1 if number > 0,0 if equal zero,-1 if number <0
+ *      Return: 1if number > 0,0 if equal zero,-1 if number < 0
  *      by a.shazally.
 **/
 int print_sign(int n)
