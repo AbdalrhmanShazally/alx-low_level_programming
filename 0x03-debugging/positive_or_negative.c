67c0222331e310804014b85f87e2cb06555b0045
AbdalrhmanShazally <ashazally@hotmail.com>
2023-08-17 00:21:49 -0700

    fix Betty coding

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -4,15 +4,14 @@
 #include "main.h"
 /* more headers goes there */
 /**
- *  * main - Entry point
- *  Description:assign a random number to the variable n
- *
+ *  * positive_or_negative - Entry point
+ *  Description:test number positive/negative or zero
+ *  @i is the number needs to be tested
  *      The output of the program should be:
 *       The number, followed by
 *       if the number is greater than 0: is positive
 *       if the number is 0: is zero
 *       if the number is less than 0: is negative
- *  Return: Always 0 (Success)
 **/
 void positive_or_negative(int i)
 {
