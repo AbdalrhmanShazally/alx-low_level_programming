696c4e6211e911dc70491094b0e2326d8f584535
AbdalrhmanShazally <ashazally@hotmail.com>
2023-08-18 00:59:26 -0700

    add 100 prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+/**
+* main - Entry point
+* Return: always 0
+*/
+
+int main(void)
+{
+long int number;
+long int max;
+long int counter;
+
+number = 612853475143;
+max = -1;
+
+while (number % 2 == 0)
+{
+max = 2;
+number = number / 2;
+}
+for (counter = 3; counter <= sqrt(number); counter = counter + 2)
+{
+while (number % counter == 0)
+{
+max = counter;
+number = number / counter;
+}
+}
+
+if (number > 2)
+max = number;
+
+printf("%ld\n", max);
+
+return (0);
+}
