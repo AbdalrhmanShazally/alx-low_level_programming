e8bf3bbeff44ad8c1422793fa52e65a26962675f
AbdalrhmanShazally <ashazally@hotmail.com>
2023-08-21 02:35:16 -0700

    fix issue with 6-puts.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,10 +12,13 @@ void puts2(char *str)
 	int length = strlen(str);
 	int i = 0;
 
-	for (i = 0; i <= length; i++)
+	if (length != 0)
+	{
+	for (i = 0; i < length; i++)
 	{
 		if (i % 2 == 0)
 			_putchar(str[i]);
 	}
 	_putchar('\n');
+	}
 }
