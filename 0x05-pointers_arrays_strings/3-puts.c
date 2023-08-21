0dea314c778cac968b333a833e1c036a1095235c
AbdalrhmanShazally <ashazally@hotmail.com>
2023-08-21 00:59:20 -0700

    add file 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _puts - prints a string, followed by a new line
+* @str: the String to be printed
+* Return: void
+*/
+
+void _puts(char *str)
+{
+	puts(str);
+	_putchar('\n');
+}
