1f5cede1fa26e4215d07d56a8feec77b7bc9e29a
AbdalrhmanShazally <ashazally@hotmail.com>
2023-08-24 16:21:24 -0700

    add file 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _strcmp - ompares two strings
+* @s1: first string
+* @s2: secoend string
+* Return: 0 or difference
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+	if (s1[i] != s2[i])
+	{
+	return (s1[i] - s2[i]);
+	}
+	i++;
+	}
+
+	return (0);
+}
